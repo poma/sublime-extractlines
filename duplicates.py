634b5f185372fc51866aad8b158d278d01bb11c3
Poma <Semenov.Roman@mail.ru>
2017-04-01 13:48:02 +0300

    join with newline, fix a bug

--- duplicates.py
+++ duplicates.py
@@ -2,12 +2,6 @@ import sublime
 import sublime_plugin
 
 
-class ExtractDuplicateLinesCommand(ExtractUniqueLinesCommand):
-
-    def run(self, edit):
-        self._run(edit, True)
-        
-
 class ExtractUniqueLinesCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
@@ -21,7 +15,7 @@ class ExtractUniqueLinesCommand(sublime_plugin.TextCommand):
 
         lines = self.get_all_lines()
         lines = self.filter(lines, duplicate)
-        text = ''.join(lines)
+        text = '\n'.join(lines)
 
         results_view.run_command('append', {'characters': text, 'force': True, 'scroll_to_end': False})
         results_view.set_syntax_file(self.view.settings().get('syntax'))
@@ -30,7 +24,7 @@ class ExtractUniqueLinesCommand(sublime_plugin.TextCommand):
         # This is the only way I found to extract a list of all lines, probably there is a more optimal one
         # Also is could be better to take newline symbol form current view settings
         ranges = self.view.lines(sublime.Region(0, self.view.size()))
-        return ['%s\n' % (self.view.substr(r)) for r in ranges]
+        return [self.view.substr(r) for r in ranges]
 
     def filter(self, lines, duplicate):
         duplicates = set(self.find_duplicates(lines));
@@ -39,3 +33,9 @@ class ExtractUniqueLinesCommand(sublime_plugin.TextCommand):
     def find_duplicates(self, lines):
         seen = set()
         return [l for l in lines if l in seen or seen.add(l)]
+
+
+class ExtractDuplicateLinesCommand(ExtractUniqueLinesCommand):
+
+    def run(self, edit):
+        self._run(edit, True)
