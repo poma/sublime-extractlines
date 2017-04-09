063ff2fbdf49ec4c1194621e8c494b53ee0d3349
poma <Semenov.Roman@mail.ru>
2017-04-09 19:42:03 +0300

    An option for in place changes

--- duplicates.py
+++ duplicates.py
@@ -1,6 +1,7 @@
 import sublime
 import sublime_plugin
 
+settings_path = 'Extract Lines.sublime-settings'
 
 class ExtractUniqueLinesCommand(sublime_plugin.TextCommand):
 
@@ -8,30 +9,30 @@ class ExtractUniqueLinesCommand(sublime_plugin.TextCommand):
         self._run(edit, False)
 
     def _run(self, edit, duplicate):
+        self.settings = sublime.load_settings(settings_path)
+        new_tab = self.settings.get('create_new_tab', False)
+        lines = self.filter(duplicate ^ (not new_tab))
+
+        if (new_tab):
+            text = '\n'.join([v for k, v in lines])
+            self.create_new_tab(text)
+        else:
+            for k, v in reversed(lines):
+                self.view.erase(edit, self.view.full_line(k))
+
+    def create_new_tab(self, text):
         results_view = self.view.window().new_file()
         results_view.set_name('Extract Results')
         results_view.set_scratch(True)
         results_view.settings().set('word_wrap', self.view.settings().get('word_wrap'))
-
-        lines = self.get_all_lines()
-        lines = self.filter(lines, duplicate)
-        text = view.line_endings().join(lines)
-
         results_view.run_command('append', {'characters': text, 'force': True, 'scroll_to_end': False})
         results_view.set_syntax_file(self.view.settings().get('syntax'))
 
-    def get_all_lines(self):
-        # This is the only way I found to extract a list of all lines, probably there is a more optimal one
-        ranges = self.view.lines(sublime.Region(0, self.view.size()))
-        return [self.view.substr(r) for r in ranges]
-
-    def filter(self, lines, duplicate):
-        duplicates = set(self.find_duplicates(lines));
-        return [l for l in lines if (l not in duplicates) ^ duplicate]
-
-    def find_duplicates(self, lines):
+    def filter(self, duplicate):
         seen = set()
-        return [l for l in lines if l in seen or seen.add(l)]
+        lines = [(l, self.view.substr(l)) for l in self.view.lines(sublime.Region(0, self.view.size()))]
+        duplicates = {v for k, v in lines if v in seen or seen.add(v)}
+        return [(k, v) for k, v in lines if (v not in duplicates) ^ duplicate]
 
 
 class ExtractDuplicateLinesCommand(ExtractUniqueLinesCommand):
